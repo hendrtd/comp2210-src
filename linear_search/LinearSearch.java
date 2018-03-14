cd90d8fcc8bea8650fda86e2c22a0dc698bde71a
Dean Hendrix <dh@auburn.edu>
2018-03-14 15:22:47 -0500

    LinearSearch: added two new implementations

--- linear_search/LinearSearch.java
+++ linear_search/LinearSearch.java
@@ -4,10 +4,36 @@
  * to class. All methods are written for arrays of ints.
  *
  * @author 	Dean Hendrix (dh@auburn.edu)
- * @version 2016-02-08
+ * @version 2018-03-14
  * 
  */
 public class LinearSearch {
+
+	/** Drives execution. */
+	public static void main(String[] args) {
+		int[] a = new int[10];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = i;
+		}
+
+		System.out.println("lsearch1: ");
+		for (int i = -1; i < a.length + 1; i++) {
+			System.out.println(i + " : " + lsearch1(a, i));
+		}
+		System.out.println();
+
+		System.out.println("lsearch2: ");
+		for (int i = -1; i < a.length + 1; i++) {
+			System.out.println(i + " : " + lsearch2(a, i));
+		}
+		System.out.println();
+
+		System.out.println("lsearch3: ");
+		for (int i = -1; i < a.length + 1; i++) {
+			System.out.println(i + " : " + lsearch3(a, i, 0, a.length - 1));
+		}
+		System.out.println();
+	}
 
 	/**
 	 * Uses linear search to search for value in a. Returns the
@@ -29,5 +55,28 @@ public class LinearSearch {
 			return -1;
 		}
 	}
+
+    /**
+     * Implements linear search on an array of integers.
+     */
+    public static int lsearch2(int[] a, int target) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /** Recursive linear search on an array of integers. */
+    public static int lsearch3(int[] a, int target, int left, int right) {
+    	if (left > right) {
+    		return -1;
+    	}
+    	if (a[left] == target) {
+    		return left;
+    	}
+    	return lsearch3(a, target, left + 1, right);
+    }
 
 }
