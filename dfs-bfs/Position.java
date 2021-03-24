51f187efe329112ea856ea7e5c0056e05668f0fa
Dean Hendrix <dh@auburn.edu>
2021-03-23 22:20:03 -0500

    Add Position constructor

---
+++ dfs-bfs/Position.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Models a position in a two-dimensional square grid.
+ * A position holds content of type variable T.
+ */
+public class Position<T> {
+
+    private T content;
+    private int row;
+    private int col;
+
+    public Position() {}
+
+}
